1e2ba79744ff36caa735e7fcb78e601d82567a3a
Ken Grimes <9122006+kengrimes@users.noreply.github.com>
2023-06-07 16:54:44 -0500

    eliminate SSG deps from astro output - hack

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import { fileURLToPath } from 'url';
+import { readFileSync, writeFileSync } from 'fs';
+
+let _buildConfig = {};
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
+  integrations: [
+    {
+      name: "ssg-import-nullifier",
+      hooks: {
+        "astro:config:done": ({config}) => _buildConfig = config.build,
+        "astro:build:done": () => {
+          const entryPath = fileURLToPath(new URL(_buildConfig.serverEntry, _buildConfig.server));
+          const entry = readFileSync(entryPath, "utf8");
+          const newEntry = entry.replace("import 'trpc-panel';", "");
+          writeFileSync(entryPath, newEntry);
+        },
+      },
+    },
+  ],
   vite: {
     build: {
       // Minified files don't source map correctly in Cloudflare Workers
       // This helps with debugging by exposing the source code in dev mode
       minify: import.meta.env.PROD,
-    }
-  }
+    },
+  },
 });
